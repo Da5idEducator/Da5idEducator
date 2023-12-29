93d6592cfc28f6bf2104275caaf837422ef904ad
David Adamcsik <150142703+Da5idEducator@users.noreply.github.com>
2023-12-29 13:52:11 +0100

    Styling calculator

--- script/konverterScript.js
+++ script/konverterScript.js
@@ -1,3 +1,7 @@
 function fontKilogrammKonverter(valNUM) {
     document.getElementById("outputKilogramm").innerHTML = valNUM * 0.453592;
 }
+
+function kilogrammFontKonverter(valNUM) {
+    document.getElementById("outputFont").innerHTML = valNUM * 2.204623;
+}
