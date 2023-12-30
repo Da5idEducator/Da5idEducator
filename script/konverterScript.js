4c2a0db1f6fc9d45818519f33ad6ba1455c389ed
David Adamcsik <150142703+Da5idEducator@users.noreply.github.com>
2023-12-30 16:50:31 +0100

    Updated calculators

--- script/konverterScript.js
+++ script/konverterScript.js
@@ -1,7 +1,28 @@
+function round(value, precision) {
+        var multiplier = Math.pow(10, precision || 0);
+        return Math.round(value * multiplier) / multiplier;
+}
+
 function fontKilogrammKonverter(valNUM) {
-    document.getElementById("outputKilogramm").innerHTML = valNUM * 0.453592;
+    document.getElementById("outputKilogramm").innerHTML = round((valNUM / 2.204623), 3);
 }
 
 function kilogrammFontKonverter(valNUM) {
-    document.getElementById("outputFont").innerHTML = valNUM * 2.204623;
+    document.getElementById("outputFont").innerHTML = round((valNUM * 2.204623), 3);
+}
+
+function inchCmKonverter(valNUM) {
+    document.getElementById("outputCm").innerHTML = round((valNUM * 2.54), 3);
+}
+
+function cmInchKonverter(valNUM) {
+    document.getElementById("outputInch").innerHTML = round((valNUM / 2.54), 3);
+}
+
+function feetMeterKonverter(valNUM) {
+    document.getElementById("outputMeter").innerHTML = round((valNUM / 3.28084), 3);
+}
+
+function meterFeetKonverter(valNUM) {
+    document.getElementById("outputFeet").innerHTML = round((valNUM * 3.28084), 3);
 }
